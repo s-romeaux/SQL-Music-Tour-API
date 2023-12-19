25eb974827fd826c1139b075683927a55c23e985
S-Romeaux <sal.4845@gmail.com>
2023-12-18 21:38:49 -0800

    Finished Name LookUp

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+
+
 
 // ROOT
 app.get('/', (req, res) => {
@@ -13,6 +18,11 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
