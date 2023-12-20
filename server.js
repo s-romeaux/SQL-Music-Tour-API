8764b9cab6d8ec3d5a13a3389e3d55f756daf796
S-Romeaux <sal.4845@gmail.com>
2023-12-19 22:48:06 -0800

    Controllers Events and Stages added

--- server.js
+++ server.js
@@ -19,9 +19,15 @@ app.get('/', (req, res) => {
     })
 })
 
-// CONTROLLERS 
+// CONTROLLERS  
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 
 // LISTEN
