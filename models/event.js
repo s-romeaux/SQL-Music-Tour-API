6d19f420be9d336eda6e30341ca6ad881c699dce
S-Romeaux <sal.4845@gmail.com>
2023-12-20 00:25:58 -0800

    Define Relationships

--- models/event.js
+++ models/event.js
@@ -1,30 +1,37 @@
 'use strict'
 const {
     Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-    class Band extends Model {
+    class Event extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet, SetTime }) {
-        // meet and greets
-        Band.hasMany(MeetGreet, {
-            foreignKey: "band_id",
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
+        // stages
+        Event.belongsToMany(Stage, {
+            foreignKey: "event_id",
+            as: "stages",
+            through: StageEvent
+        })
+    
+        // meet and greets 
+        Event.hasMany(MeetGreet, {
+            foreignKey: "event_id",
             as: "meet_greets"
         })
 
         // set times 
-        Band.hasMany(SetTime, {
-            foreignKey: "band_id",
-            set_times: "set_times"
+        Event.hasMany(SetTime, {
+            foreignKey: "event_id",
+            as: "set_times"
         })
     }
 }
-Band.init({
-    band_id: {
+Event.init({
+    event_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
@@ -33,11 +40,11 @@ Band.init({
         type: DataTypes.STRING,
         allowNull: false
     },
-    genre: {
-        type: DataTypes.TEXT,
+    date: {
+        type: DataTypes.DATE,
         allowNull: false
     },
-    available_start_time: {
+    start_time: {
         type: DataTypes.DATE,
         allowNull: false
     },
@@ -47,9 +54,9 @@ Band.init({
     }
 }, {
     sequelize,
-    modelName: 'Band',
-    tableName: 'bands',
+    modelName: 'Event',
+    tableName: 'events',
     timestamps: false
-});
-return Band;
-};
+})
+return Event
+}
